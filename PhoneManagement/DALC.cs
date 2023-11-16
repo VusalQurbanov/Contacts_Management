e03b2967f222170261b08cd408b809aa0dc2f5dd
VusalQurbanov <qurbanovusal123@gmail.com>
2023-11-16 14:57:36 +0400

    first

---
+++ PhoneManagement/DALC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneManagement
+{
+    internal class DALC
+    {
+        public static string GetStringConnection()
+        {
+            string connectionString = @"Data Source=DESKTOP-EBTHQ2Q\SQLEXPRESS;Initial Catalog=Vusal;Integrated Security=True";
+            return connectionString;
+        }
+    }
+}
